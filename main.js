36627209504d4e8f76049086a3dfa242e3f0d85f
Mingrui Lu <rhys.211sfs@qq.com>
2020-03-19 16:06:20 +0800

    增加了分类功能

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
 let length, width, diagonal, ratio, ppi;
 let devicesData = [
+    ['Android',
+        [
+            ['Mi 10', 2340, 1080, 6.67],
+            ['Mi 10 Pro', 2340, 1080, 6.67],
+            ['Redmi 6', 1440, 720, 5.45],
+            ['Redmi 7', 1520, 720, 6.26],
+            ['Redmi 7A', 1440, 720, 5.45],
+            ['Redmi 8', 1520, 720, 6.22],
+            ['Redmi Note 8', 2340, 1080, 6.3],
+            ['Redmi Note 8 Pro', 2340, 1080, 6.53],
+            ['OnePlus 7T', 2400, 1080, 6.55],
+            ['OnePlus 7T Pro', 3120, 1440, 6.67],
+            ['Sony Xperia 5', 2520, 1080, 6.1],
+            ['Sony Xperia 1', 3840, 1644, 6.5],
+            ['Sony Xperia 10 Plus', 2520, 1080, 6.5],
+            ['Sony Xperia XZ3', 2880, 1440, 6]
+        ]
+    ],
+    ['iPhone',
+        [
+            ['iPhone 4', 960, 240, 3.5],
+            ['iPhone 4s', 960, 240, 3.5],
+            ['iPhone 5', 1136, 640, 4],
+            ['iPhone 5s', 1136, 640, 4],
+            ['iPhone 6', 1334, 750, 4.7],
+            ['iPhone 6 Plus', 1920, 1080, 5.5],
+            ['iPhone 6s', 1334, 750, 4.7],
+            ['iPhone 6s Plus', 1920, 1080, 5.5],
+            ['iPhone 7', 1334, 750, 4.7],
+            ['iPhone 7 Plus', 1920, 1080, 5.5],
+            ['iPhone 8', 1334, 750, 4.7],
+            ['iPhone 8 Plus', 1920, 1080, 5.5],
+            ['iPhone X', 2436, 1125, 5.8],
+            ['iPhone XR', 1792, 828, 6.1],
+            ['iPhone XS', 2436, 1125, 5.8],
+            ['iPhone XS Max', 2688, 1242, 6.5],
+            ['iPhone 11', 1792, 828, 6.1],
+            ['iPhone 11 Pro', 2436, 1125, 5.8],
+            ['iPhone 11 Pro Max', 2688, 1242, 6.5]
+        ]
+    ]
+];
+
+
+/*let devicesData = [
     ['Mi 10', 2340, 1080, 6.67],
     ['Mi 10 Pro', 2340, 1080, 6.67],
     ['Redmi 6', 1440, 720, 5.45],
@@ -33,8 +78,7 @@ let devicesData = [
     ['iPhone 11', 1792, 828, 6.1],
     ['iPhone 11 Pro', 2436, 1125, 5.8],
     ['iPhone 11 Pro Max', 2688, 1242, 6.5],
-
-]
+]*/
 function update() {
     length = document.querySelector('#length').value
     width = document.querySelector('#width').value
@@ -56,9 +100,9 @@ function getGCF(a, b) {
             return i
 }
 
-function calc(){
+function calc() {
     let gcf = getGCF(length, width)
-    ratio = `${length/gcf} : ${width/gcf}`
+    ratio = `${length / gcf} : ${width / gcf}`
     ppi = Math.round((length ** 2 + width ** 2) ** 0.5 / diagonal)
     document.querySelector('#ppi').innerHTML = ppi
     document.querySelector('#ratio').innerHTML = ratio
